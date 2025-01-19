eb8f1f1895b65cf1abcca185b0f937a51af11478
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2025-01-19 15:36:30 +0000

    change svg path

--- src/js/map.js
+++ src/js/map.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const mapContainer = document.getElementById("map-container");
   
     // Load the SVG dynamically
-    fetch("/map/worldmap.svg")
+    fetch("map/worldmap.svg")
       .then((response) => response.text())
       .then((svgData) => {
         mapContainer.innerHTML = svgData;
